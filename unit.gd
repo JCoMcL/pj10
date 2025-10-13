107ffd87f4d5872a9478a4d2a19e7329fe316d13
jcomcl <j@jcom.cl>
2025-10-13 22:42:56 +0100

    fix and error causes unit expiration to double-trigger

--- unit.gd
+++ unit.gd
@@ -8,17 +8,23 @@ extends CharacterBody2D
 
 signal expire
 
+var alive = true
 func _expire():
-	expire.emit()
+	assert(alive)
+	alive = false
 	if auto_free:
 		queue_free()
+	expire.emit()
 
 func _hit():
 	_expire()
 
 func _on_exit_play_area():
-	if expire_outside_play_area:
+	if expire_outside_play_area and alive:
 		_expire()
+
+func _enter_tree() -> void:
+	alive = true
 
 func _physics_process(delta: float) -> void:
 	if not Engine.is_editor_hint():
